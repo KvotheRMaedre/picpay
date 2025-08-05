d0cd625336f68179c204ff54c318c40d2204d47b
Ítalo H. <italodssouza.backup@gmail.com>
2025-08-05 11:12:05 -0300

    Handling exception for MethodArgumentNotValidException

--- src/main/java/tech/kvothe/picpay/controller/RestExceptionHandler.java
+++ src/main/java/tech/kvothe/picpay/controller/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package tech.kvothe.picpay.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tech.kvothe.picpay.exception.PicPayException;
@@ -11,4 +13,20 @@ public class RestExceptionHandler {
     public ProblemDetail handlePicPayException(PicPayException picPayException) {
         return picPayException.toProblemDetail();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        var fieldErrors = methodArgumentNotValidException.getFieldErrors()
+                .stream()
+                .map(fieldError -> new InvalidParam(fieldError.getField(), fieldError.getDefaultMessage()))
+                .toList();
+
+        var problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("Your request parameters didn't validate.");
+        problemDetail.setProperty("invalid-params", fieldErrors);
+
+        return problemDetail;
+    }
+
+    private record InvalidParam(String name, String reason){}
 }
