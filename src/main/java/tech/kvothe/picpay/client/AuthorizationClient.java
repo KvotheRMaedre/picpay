ff3132d58fb162d0780938b4a5c59af84a2b9954
Ítalo H. <italodssouza.backup@gmail.com>
2025-08-05 13:42:01 -0300

    Configuring the open feign and the clients for notification and authorization

---
+++ src/main/java/tech/kvothe/picpay/client/AuthorizationClient.java
@@ -0,0 +1,16 @@
+package tech.kvothe.picpay.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import tech.kvothe.picpay.client.dto.AuthorizationResponse;
+
+@FeignClient(
+        name = "AuthorizationClient",
+        url = "${client.authorization-service.url}"
+)
+public interface AuthorizationClient {
+
+    @GetMapping
+    ResponseEntity<AuthorizationResponse> isAuthorized();
+}
---
+++ src/main/java/tech/kvothe/picpay/client/NotificationClient.java
@@ -0,0 +1,16 @@
+package tech.kvothe.picpay.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import tech.kvothe.picpay.entity.Transfer;
+
+@FeignClient(
+        name = "NotificationClient",
+        url = "${client.notification-service.url}"
+)
+public interface NotificationClient {
+    @PostMapping
+    ResponseEntity<Void> sendNotification(@RequestBody Transfer transfer);
+}
